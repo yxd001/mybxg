cfe614aa7aae34ba85a4cfa91fe45f2174993a13
zhangsan <2540592245@qq.com>
2017-09-20 14:49:23 +0800

    完成讲师列表展示功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,10 +1,11 @@
+// 讲师列表功能
 define(['jquery','template'],function ($,template) {
   $.ajax({
     type:'get',
     url:'/api/teacher',
     dataType:'json',
     success:function(data){
-      console.log(data);
+      //console.log(data);
       var html = template('template',{list:data.result});
       $('#teacheinfo').html(html);
     }
