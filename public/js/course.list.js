1d35c4e1807f2577bcd159623c35b0fdc7bb3d2a
zhangsan <2540592245@qq.com>
2017-09-25 01:05:57 +0800

    完成课程列表展示功能

--- public/js/course.list.js
+++ public/js/course.list.js
@@ -1,4 +1,14 @@
 define(['jquery','template','utile'],function($,template,utile){
 //设置导航栏菜单选中
     utile.setMenu(location.pathname);
+
+    $.ajax({
+        type:'get',
+        url:'/api/course',
+        dataType:'json',
+        success:function(data){
+            var html = template('coursetcl',{list:data.result});
+            $('#courseIofo').html(html);
+        }
+    })
 })
